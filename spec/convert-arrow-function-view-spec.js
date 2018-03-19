15e9cf009b63b10c21a509dd6c241359c2daa13a
Nate Festinger <nate.festinger@gmail.com>
2018-03-19 00:51:29 -0700

    Initial commit

---
+++ spec/convert-arrow-function-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ConvertArrowFunctionBView from '../lib/convert-arrow-function-view';
+
+describe('ConvertArrowFunctionBView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
